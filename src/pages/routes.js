c3412e1fb9df500881abc04c4647a63c0d3b22b2
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-08-21 18:42:09 +0200

    refactor

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-import { Switch, Route, /* Redirect */ } from 'react-router-dom';
+import { Switch, Route, HashRouter, Redirect } from 'react-router-dom';
 
-/* import ErrorPage from './error'; */
+import ErrorPage from './error';
 import HomePage from './home';
 import LoginPage from './login';
 import TokenPage from './token';
 
 const Routes = () => (
-  <Switch>
-    <Route exact path='/' component={LoginPage} />
-    <Route exact path='/token' component={TokenPage} />
-    <Route exact path='/home' component={HomePage} />
-    {/* <Route exact path='/error' component={ErrorPage} />
-    <Redirect to="/error" /> */}
-  </Switch>
+  <HashRouter>
+    <Switch>
+      <Route exact path='/' component={LoginPage} />
+      <Route exact path='/token' component={TokenPage} />
+      <Route exact path='/home' component={HomePage} />
+      <Route exact path='/error' component={ErrorPage} />
+      <Redirect to="/error" />
+    </Switch>
+  </HashRouter>
 );
 
 export default Routes;
