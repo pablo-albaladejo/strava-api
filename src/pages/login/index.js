1e55b94be900992aa11204d141eedc1ec37c62b2
Pablo Albaladejo <pablo.albaladejo@guidesmiths.com>
2020-08-21 19:30:21 +0200

    encoding fixed

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import MainLayout from '../../components/layout/main';
 const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI } = process.env;
-//console.log(process.env)
+
 const LoginPage = () => (
     <MainLayout>
         <div className="App">
-            <a href={`https://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URI}&approval_prompt=force&scope=read`}>Strava</a>
+            <a href={`https://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REACT_APP_REDIRECT_URI)}&approval_prompt=force&scope=read`}>Strava</a>
         </div>
     </MainLayout>
 );
