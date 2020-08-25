82de2e166f753f8d99a0307528a5b7d6910f1758
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-25 10:55:52 +0000

    Deploy pablo-albaladejo/strava-api to github.com/pablo-albaladejo/strava-api.git:gh-pages

---
+++ precache-manifest.094e33fb774523e8dd9f6f7fae1f0fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d88bd5e4f6102211ef60e74e215edfaa",
+    "url": "https://pablo-albaladejo.github.io/strava-api/index.html"
+  },
+  {
+    "revision": "cb169d26b349721724ce",
+    "url": "https://pablo-albaladejo.github.io/strava-api/static/css/main.a7d79edc.chunk.css"
+  },
+  {
+    "revision": "caad04f01bfd54005cdc",
+    "url": "https://pablo-albaladejo.github.io/strava-api/static/js/2.7e0dff2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://pablo-albaladejo.github.io/strava-api/static/js/2.7e0dff2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb169d26b349721724ce",
+    "url": "https://pablo-albaladejo.github.io/strava-api/static/js/main.223d274f.chunk.js"
+  },
+  {
+    "revision": "ccf2d0589e423017aa6f",
+    "url": "https://pablo-albaladejo.github.io/strava-api/static/js/runtime-main.cf7700e0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://pablo-albaladejo.github.io/strava-api/precache-manifest.094e33fb774523e8dd9f6f7fae1f0fe5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://pablo-albaladejo.github.io/strava-api/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
